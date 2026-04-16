78dabe31b439e7ee3b12e5344a1bf3b06bdc55e1
xSizzlingHotx <yannik.raetzke@proton.me>
2026-04-17 00:53:55 +0200

    add pages/index.js

---
+++ pages/index.js
@@ -0,0 +1,7 @@
+import { useRouter } from 'next/router';
+import { useEffect } from 'react';
+export default function Index() {
+  const router = useRouter();
+  useEffect(() => { router.replace('/map'); }, []);
+  return null;
+}
